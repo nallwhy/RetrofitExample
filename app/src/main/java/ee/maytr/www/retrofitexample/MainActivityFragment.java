ff7969d897e5eb6442138cfc8c0e4ebfdbc4dd45
json <json@maytr.ee>
2015-12-28 23:59:19 +0900

    Retrofit Example

---
+++ app/src/main/java/ee/maytr/www/retrofitexample/MainActivityFragment.java
@@ -0,0 +1,37 @@
+package ee.maytr.www.retrofitexample;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = (Button)view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).weatherManager.getWeather();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/ee/maytr/www/retrofitexample/WeatherManager.java
@@ -0,0 +1,86 @@
+package ee.maytr.www.retrofitexample;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+import retrofit.http.Body;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+/**
+ * Created by json on 2015. 12. 28..
+ */
+public class WeatherManager {
+    private Context context = null;
+    private Retrofit retrofit = null;
+    private WeatherService weatherService = null;
+
+    public WeatherManager(Context context) {
+        this.context = context;
+        this.retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl("http://api.openweathermap.org/data/2.5/").build();
+        this.weatherService = retrofit.create(WeatherService.class);
+    }
+
+    public interface WeatherService {
+        @POST("weather")
+        Call<Dummy> getWeather(@Query("q") String city, @Query("APPID") String api_key);
+    }
+
+    Callback<Dummy> weatherCallback = new Callback<Dummy>() {
+        @Override
+        public void onResponse(Response<Dummy> response, Retrofit retrofit) {
+            Toast.makeText(context, response.body().toString(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            Toast.makeText(context, t.toString(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    public void getWeather() {
+        Call<Dummy> call = weatherService.getWeather("London", "");
+        call.enqueue(weatherCallback);
+    }
+
+    private class Dummy {
+        public Weather main;
+
+        public Dummy(Weather main) {
+            this.main = main;
+        }
+
+        @Override
+        public String toString() {
+            return this.main.toString();
+        }
+    }
+
+    private class Weather {
+        public double temp = 0.0;
+        public double pressure = 0.0;
+        public double humidity = 0.0;
+        public double temp_min = 0.0;
+        public double temp_max = 0.0;
+
+        public Weather(double temp, double pressure, double humidity, double temp_min, double temp_max) {
+            this.temp = temp;
+            this.pressure = pressure;
+            this.humidity = humidity;
+            this.temp_min = temp_min;
+            this.temp_max = temp_max;
+        }
+
+        @Override
+        public String toString() {
+            return "temp: " + temp + ", pressure: " + pressure + ", humidity: " + humidity + ", temp_min: " + temp_min + ", temp_max: " + temp_max;
+        }
+    }
+}
